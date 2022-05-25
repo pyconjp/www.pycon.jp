45b3799a1560bb42daa5e5eedb90abf3a9966c02
Takanori Suzuki <takanori@takanory.net>
2022-05-25 21:47:29 +0900

    ISSHA-2572 add settings for footer

--- source/conf.py
+++ source/conf.py
@@ -58,6 +58,10 @@ html_theme_path = ['_themes']
 
 html_title = 'PyCon JP'
 
+html_last_updated_fmt = '%Y-%m-%d'
+
+html_show_copyright = False
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
