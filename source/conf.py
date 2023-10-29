e4dfc4eab0f78a67465ec842191479cf82403c4f
Takanori Suzuki <takanori@takanory.net>
2023-10-29 14:52:59 +0900

    ISSHA-3067 get font lists on read the docs image

--- source/conf.py
+++ source/conf.py
@@ -95,3 +95,10 @@ linkcheck_ignore = [
     r"https://docs.google.com/.*",
     r"http://trac.pycon.jp/.*",
 ]
+
+import matplotlib.font_manager as fm
+import pprint
+
+font_list = [f.name for f in fm.fontManager.ttflist]
+
+pprint.pprint(font_list)
